285bf9961d13699d019a676b92defab586505b04
deepakfeb5 <60507470+deepakfeb5@users.noreply.github.com>
2026-03-16 17:41:30 +0530

    Update ResultsTable.tsx

--- web/components/ResultsTable.tsx
+++ web/components/ResultsTable.tsx
@@ -1,7 +1,6 @@
-import ResultsTable from "../components/ResultsTable";
 export default function ResultsTable({ rows }: { rows: any[] }) {
   return (
-    <table border={1} cellPadding={6} style={{ marginTop: 20 }}>
+    <table border={1} cellPadding={6} style={{ marginTop: 20, width: "100%", borderCollapse: "collapse" }}>
       <thead>
         <tr>
           <th>Part Number</th>
@@ -15,7 +14,6 @@ export default function ResultsTable({ rows }: { rows: any[] }) {
           <th>Error</th>
         </tr>
       </thead>
-
       <tbody>
         {rows.map((r, i) => (
           <tr key={i}>
